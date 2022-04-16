a92466ba7f13e2908947c31de91723ec208a7311
kcdove <kayojukwu@gmail.com>
2022-04-16 06:41:32 -0700

    pls commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,18 @@
  *
  * Return: Always 0.
  */
+int main(void)
+{
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 
-          _putchar (i + '0');
-        _putchar ('\n');
+          putchar (i + '0');
+        putchar ('\n');
 
 
+}
+return (0);
 }
